31a1b6e1be8d2b1ba6336ed3cf2fa0eee8880c84
mohamed magdy ismael <mohamed.magdy@vezeeta.com>
2022-12-13 16:08:23 +0200

    Updates

---
+++ precache-manifest.36305e4e8b5006c1b47ed6a98923e584.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e35774c062991b343e7a",
+    "url": "/traktv-search-engine/static/js/main.e35774c0.chunk.js"
+  },
+  {
+    "revision": "00ee18c5a7e7a81a539e",
+    "url": "/traktv-search-engine/static/js/runtime~main.00ee18c5.js"
+  },
+  {
+    "revision": "7066f7f31d2b48872cf3",
+    "url": "/traktv-search-engine/static/js/2.7066f7f3.chunk.js"
+  },
+  {
+    "revision": "5437c4ee795a567ff51c5d1161f8b0e6",
+    "url": "/traktv-search-engine/static/media/movieFilmingSmall.5437c4ee.jpg"
+  },
+  {
+    "revision": "395ab3d7c4e51b7f9764caca42d7fb25",
+    "url": "/traktv-search-engine/static/media/ILaaIYY0_400x400.395ab3d7.png"
+  },
+  {
+    "revision": "d245ed4b667135654ae0c22db2d91099",
+    "url": "/traktv-search-engine/static/media/headerbanner.d245ed4b.jpg"
+  },
+  {
+    "revision": "0d60d5bb023403a418b6ea687233a77c",
+    "url": "/traktv-search-engine/index.html"
+  }
+];
